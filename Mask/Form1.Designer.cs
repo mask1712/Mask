0a7c3b8c87d8846b02a2d6d345df6adbb3041365
make <598705799@qq.com>
2019-05-23 16:36:07 +0800

    添加项目文件。

---
+++ Mask/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Mask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSourceText = new System.Windows.Forms.TextBox();
+            this.txtResultText = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSourceText
+            // 
+            this.txtSourceText.Location = new System.Drawing.Point(12, 12);
+            this.txtSourceText.Multiline = true;
+            this.txtSourceText.Name = "txtSourceText";
+            this.txtSourceText.Size = new System.Drawing.Size(485, 175);
+            this.txtSourceText.TabIndex = 0;
+            // 
+            // txtResultText
+            // 
+            this.txtResultText.Location = new System.Drawing.Point(12, 222);
+            this.txtResultText.Multiline = true;
+            this.txtResultText.Name = "txtResultText";
+            this.txtResultText.Size = new System.Drawing.Size(485, 205);
+            this.txtResultText.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(81, 193);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "加密";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(295, 193);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "解密";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtResultText);
+            this.Controls.Add(this.txtSourceText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSourceText;
+        private System.Windows.Forms.TextBox txtResultText;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+    }
+}
+
---
+++ Mask/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSourceText.Text))
+            {
+                MessageBox.Show("没数据加毛密-_-!");
+                return;
+            }
+            else
+            {
+                txtResultText.Text = AESEncryptHelper.Encrypt(txtSourceText.Text);
+            }
+        }
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSourceText.Text))
+            {
+                MessageBox.Show("没数据解毛密-_-!");
+                return;
+            }
+            else if (!IsBase64Formatted(txtSourceText.Text))
+            {
+                MessageBox.Show("别逗了,我只认识被我加过密的？");
+                return;
+            }
+            else
+            {
+                txtResultText.Text = AESEncryptHelper.Decrypt(txtSourceText.Text);
+            }
+        }
+
+        public static bool IsBase64Formatted(string input)
+        {
+            try
+            {
+                Convert.FromBase64String(input);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
